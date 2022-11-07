cd8fcdf4a85c7f54a08a061ec576da5df3680b5a
cy-moi <congyao119@gmail.com>
2022-11-07 19:08:32 +0100

    try selfie segmentation

--- entriesMap.js
+++ entriesMap.js
@@ -2,7 +2,8 @@ const entriesMap = {
   hand: './src/handDemo/index',
   faceDraw: './src/faceDraw/index',
   handFire: './src/handFire/index',
-  hand3D: './src/hand3D/index'
+  hand3D: './src/hand3D/index',
+  segment: './src/segmentDemo/index'
 }
 
 module.exports = entriesMap

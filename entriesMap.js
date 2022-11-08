7490bdae01ca0a585efc7ba9e73a9a6658f6ad4b
cy-moi <congyao119@gmail.com>
2022-11-08 03:07:12 +0100

    remove segment experiment

--- entriesMap.js
+++ entriesMap.js
@@ -3,7 +3,6 @@ const entriesMap = {
   faceDraw: './src/faceDraw/index',
   handFire: './src/handFire/index',
   hand3D: './src/hand3D/index',
-  segment: './src/segmentDemo/index'
 }
 
 module.exports = entriesMap

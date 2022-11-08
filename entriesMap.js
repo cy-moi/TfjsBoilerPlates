2637dde1afd10773c0b7d88d0ed23f29bad10e4f
cy-moi <congyao119@gmail.com>
2022-11-08 01:00:18 +0100

    remove entry

--- entriesMap.js
+++ entriesMap.js
@@ -1,5 +1,5 @@
 const entriesMap = {
-  hand: './src/handDemo/index',
+  // hand: './src/handDemo/index',
   // faceDraw: './src/faceDraw/index',
   // handFire: './src/handFire/index',
   hand3D: './src/hand3D/index',
